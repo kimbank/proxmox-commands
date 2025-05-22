de77e45756368e5b77d3e5d3a2e6f6743662b55f
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-05-23 08:49:46 +0900

    add: debian uptime kuma

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # proxmox-commands
 
+prerequisite
+
+- curl
+
 ## [Debian] docker installation
 
 ```sh
@@ -16,4 +20,12 @@ curl -s https://raw.githubusercontent.com/kimbank/proxmox-commands/main/ubuntu-d
 
 ```sh
 curl -s https://raw.githubusercontent.com/kimbank/proxmox-commands/main/ubuntu-jdk17.sh | bash
+```
+
+## [Debian] uptime kuma (non-docker)
+
+> https://github.com/louislam/uptime-kuma?tab=readme-ov-file#-non-docker
+
+```sh
+curl -s https://raw.githubusercontent.com/kimbank/proxmox-commands/main/debian-uptime-kuma.sh | bash
 ```
---
+++ debian-uptime-kuma.sh
@@ -0,0 +1,44 @@
+apt update && sudo apt upgrade -y
+
+# git
+apt install -y git
+
+# nodejs
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+apt install -y nodejs
+
+# pm2
+npm install pm2 -g && pm2 install pm2-logrotate
+
+# clone uptime kuma git repository
+if [ ! -d "uptime-kuma" ]; then
+  git clone https://github.com/louislam/uptime-kuma.git
+fi
+cd uptime-kuma
+npm run setup
+
+echo
+echo "Uptime Kuma installed"
+echo "directory: $(pwd)"
+echo
+
+
+cat <<EOF
+다음 단계 안내:
+
+# Option 1. Try it
+node server/server.js
+
+# (Recommended) Option 2. Run in the background using PM2
+pm2 start server/server.js --name uptime-kuma
+
+Then Uptime Kuma is now running on http://localhost:3001
+
+> More useful PM2 Commands
+
+# If you want to see the current console output
+pm2 monit
+
+# If you want to add it to startup
+pm2 save && pm2 startup
+EOF
